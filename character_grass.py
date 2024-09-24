11b4a3d386e6954b3c3959d135dd44dd3c632658
Hyochang-Son@main notebook <dagdf20@naver.com>
2024-09-24 10:01:52 +0900

    캐릭터 출력

--- character_grass.py
+++ character_grass.py
@@ -1,5 +1,9 @@
 from pico2d import *
 
+open_canvas()
+
+grass=load_image('grass.png')
+boy=load_image('character.png')
 
 def run_rectangle():
     print('Rectangle')
@@ -7,17 +11,17 @@ def run_rectangle():
 
 def run_circle():
     print('Circle')
+
+    clear_canvas_now()
+    boy.draw_now(400,300)
+    delay(1)
+    
     pass
-open_canvas()
-
-# fill
-
-grass=load_image('grass.png')
-boy=load_image('character.png')
 
 while True:
     run_circle()
     run_rectangle()
+    break
 
     
 close_canvas()
