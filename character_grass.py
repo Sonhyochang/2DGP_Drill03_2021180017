1bf9bac702a2e13a04b411f5f7f18176f737638f
Hyochang-Son@main notebook <dagdf20@naver.com>
2024-09-24 09:53:45 +0900

    프린트 통해서 정상 루프 동작 확인

--- character_grass.py
+++ character_grass.py
@@ -2,9 +2,11 @@ from pico2d import *
 
 
 def run_rectangle():
+    print('Rectangle')
     pass
 
 def run_circle():
+    print('Circle')
     pass
 open_canvas()
 
@@ -14,7 +16,8 @@ grass=load_image('grass.png')
 boy=load_image('character.png')
 
 while True:
-    run_rectangle()
     run_circle()
+    run_rectangle()
+
     
 close_canvas()
